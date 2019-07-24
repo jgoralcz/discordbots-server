4425e47a9406d18cc0c74c63dded7a2238e3c955
jgoralcz <jgoralcz@asu.edu>
2019-07-24 15:07:21 -0700

    added initial testing

---
+++ db.js
@@ -0,0 +1,90 @@
+const { Pool } = require('pg');
+const { db } = require('./config.json');
+
+const pool = new Pool(db);
+
+/**
+ * updates a user when they vote.
+ * @param userID the user's id.
+ * @param points the user's bank points to update.
+ * @param vote_date the date to add to the vote.
+ * @param streak_vote_date the streak date to add
+ * @returns {Promise<*>}
+ */
+const updateUserBankPointsVote = async (userID, points, vote_date, streak_vote_date) => {
+  const client = await pool.connect();
+  try {
+    return await client.query(`
+      UPDATE "clientsTable"
+      SET "bankPoints" = "bankPoints" + $2, vote_date = $3, streak_vote_date = $4, vote_enabled = TRUE, streak_vote = streak_vote + 1
+      WHERE "userId" = $1
+      RETURNING *;
+    `, [userID, points, vote_date, streak_vote_date]);
+  } finally {
+    client.release();
+  }
+};
+
+/**
+ * stores a new user in the database.
+ * @param userID the user's id.
+ * @returns {Promise<void>}
+ */
+const initializeGetUserInfo = async (userID) => {
+  let info = await checkUserExists(userID).catch(console.error);
+
+  if(info == null) return null;
+
+  if (info != null && info.rowCount <= 0) {
+    const startClientInfo = {
+      userId: userID,
+      prefix: config.prefix
+    };
+
+    //set and then get the info
+    info = await setUserInfo(startClientInfo);
+  }
+  return info.rows[0];
+};
+
+/**
+ * check if the user exists.
+ * @param userID the user's id.
+ * @returns {Promise<*>}
+ */
+const checkUserExists = async (userID) => {
+  const client = await pool.connect();
+  try {
+    return await client.query(`
+      SELECT null
+      FROM "clientsTable"
+      WHERE "userId" = $1;
+    `, [userID]);
+  } finally {
+    client.release();
+  }
+};
+
+/**
+ * sets the new user info
+ * @param userInfo the user's info
+ * @returns {Promise<*>}
+ */
+const setUserInfo = async (userInfo) => {
+  const client = await pool.connect();
+  try {
+    return await client.query(`
+      INSERT INTO "clientsTable" ("userId", "prefix")
+      VALUES ($1, $2)
+      ON CONFLICT("userId") DO NOTHING
+      RETURNING *;
+    `, [userInfo.userId, userInfo.prefix]);
+  } finally {
+    client.release();
+  }
+};
+
+module.exports = {
+  updateUserBankPointsVote,
+  initializeGetUserInfo
+};
---
+++ index.js
@@ -0,0 +1,49 @@
+const { dbl, streakAmount, maxStreak } = require('./config.json');
+const dblapi = require('dblapi.js');
+const db = require('./db.js');
+
+const discordBots = new dblapi(dbl.token, { webhookPort: dbl.port, webhookAuth: dbl.pass });
+
+
+discordBots.webhook.on('ready', hook => {
+  console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
+});
+discordBots.webhook.on('vote', async vote => {
+  console.log(`User with ID ${vote.user} voted:`, new Date());
+  let points = 3000;
+
+  // is weekend, 4000 points
+  if(vote != null && vote.isWeekend) {
+    points = 4000;
+  }
+
+  //get now time and add time to it
+  let now = new Date();
+  now.setHours(now.getHours() + 36);
+  now = now.toLocaleString();
+
+  try {
+    // set up new member if needed
+    const userInfo = await db.initializeGetUserInfo(vote.user);
+    const streak = (userInfo.streak_vote || 0) + 1;
+
+    // show the votes and points
+    (streak > 10) ? points += streakAmount * maxStreak : points += streakAmount * (streak - 1);
+
+    // points info (streak included now)
+    await db.updateUserBankPointsVote(vote.user, points, new Date(), now);
+
+    // only care if they voted a long time so we can record it.
+    if(streak > 20) {
+      // send to our server so that they can send it back to us.
+
+    }
+    else {
+      console.log(`${vote.user} has received ${points} points, reset their rolls, and is on a ${streak} day voting streak.`);
+    }
+
+  }
+  catch(error) {
+    console.error(error);
+  }
+});
