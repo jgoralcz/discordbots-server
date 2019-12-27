ca1d90763c3108b7692282289b65b8acd04fd427
jgoralcz <jgoralcz@asu.edu>
2019-12-27 13:25:08 -0700

    added one liner

--- src/routes/stats.js
+++ src/routes/stats.js
@@ -3,9 +3,7 @@ const discordBots = require('../dbl');
 
 app.post('/', async (req, res) => {
   const { body } = req;
-  if (!body) {
-    return res.status(400).send({ error: 'No body provided.' });
-  }
+  if (!body) return res.status(400).send({ error: 'No body provided.' });
 
   const { servers, shardID, shardCount } = body;
   if (servers == null || shardID == null || shardCount == null) {
