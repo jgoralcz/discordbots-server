587665406f5cc54f7ab05d0c7b5750b2aa764d33
jgoralcz <jgoralcz@asu.edu>
2020-03-01 14:58:34 -0700

    reflects new changes with patron

--- src/cron/MinuteTask.js
+++ src/cron/MinuteTask.js
@@ -14,38 +14,25 @@ ScheduleJob('minute', '0 * * * * *', async () => {
   await bongoBotAPI.put('/leaderboards/refresh').catch(error => logger.error(error));
 
   const now = new Date();
-  const minutes = now.getMinutes();
-  const hours = now.getHours();
+  const minute = now.getMinutes();
+  const hour = now.getHours();
 
-  // check patron level 2 for the special cases.
-  await bongoBotAPI.patch('/rolls/patron-two/reset', { minutes }).catch(error => logger.error(error));
-  await bongoBotAPI.patch('/claims/patron-two/reset', { minutes }).catch(error => logger.error(error));
+  await bongoBotAPI.patch('/rolls/reset', { minute }).catch(error => logger.error(error));
+  await bongoBotAPI.patch('/claims/reset', { hour, minute }).catch(error => logger.error(error));
 
-  if (minutes !== 0) return;
+  if (minute !== 0) return;
+  await bongoBotAPI.patch('/guilds/roll-claim-minute').catch(error => logger.error(error));
 
-  await bongoBotAPI.patch('/rolls/reset').catch(error => logger.error(error));
   // clear streaks, more or less lenient every hour.
   await bongoBotAPI.patch('/users/guilds/streaks/reset').catch(error => logger.error(error));
   await bongoBotAPI.patch('/users/guilds/streaks/votes/reset').catch(error => logger.error(error));
 
-  // reset the patron level 2 minutes
-  await bongoBotAPI.patch('/guilds/roll-claim-minute').catch(error => logger.error(error));
-
-  // third hour we reset claims for normal users.
-  if (hours % 3 === 0) {
-    await bongoBotAPI.patch('/claims/plebs/reset').catch(error => logger.error(error));
-  }
-
-  // second hour we reset claims for patrons.
-  if (hours % 2 === 0) {
-    await bongoBotAPI.patch('/claims/patron-one/reset').catch(error => logger.error(error));
-  }
-
   // reset all dailies for all users.
-  if (hours / 12 === 1) {
-    logger.log('running daily reset...');
-    await bongoBotAPI.patch('/users/dailies/reset').catch(error => logger.error(error));
-    await bongoBotAPI.delete('/guilds/lastplayed').catch(error => logger.error(error));
-    await bongoBotAPI.delete('/guilds/queue').catch(error => logger.error(error));
-  }
+  if (hour / 12 !== 1) return;
+
+  logger.log('running daily reset...');
+
+  await bongoBotAPI.patch('/users/dailies/reset').catch(error => logger.error(error));
+  await bongoBotAPI.delete('/guilds/lastplayed').catch(error => logger.error(error));
+  await bongoBotAPI.delete('/guilds/queue').catch(error => logger.error(error));
 });
