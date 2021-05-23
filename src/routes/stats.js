4853324aa48d30e0eab2b2aca770bd168dc32ca7
Joshua Goralczyk <joshgor01@gmail.com>
2021-05-23 15:44:36 -0700

    comment unused

--- src/routes/stats.js
+++ src/routes/stats.js
@@ -1,17 +1,17 @@
-const route = require('express-promise-router')();
-const discordBots = require('../dbl');
+// const route = require('express-promise-router')();
+// const discordBots = require('../dbl');
 
-route.post('/', async (req, res) => {
-  const { body } = req;
-  if (!body) return res.status(400).send({ error: 'No body provided.' });
+// route.post('/', async (req, res) => {
+//   const { body } = req;
+//   if (!body) return res.status(400).send({ error: 'No body provided.' });
 
-  const { servers, shardID, shardCount } = body;
-  if (servers == null || shardID == null || shardCount == null) {
-    return res.status(400).send({ error: 'Invalid data received. Need: servers, shardID, shardCount.', body });
-  }
+//   const { servers, shardID, shardCount } = body;
+//   if (servers == null || shardID == null || shardCount == null) {
+//     return res.status(400).send({ error: 'Invalid data received. Need: servers, shardID, shardCount.', body });
+//   }
 
-  await discordBots.postStats(servers, shardID, shardCount);
-  return res.status(200).send({ servers, shardID, shardCount });
-});
+//   await discordBots.postStats(servers, shardID, shardCount);
+//   return res.status(200).send({ servers, shardID, shardCount });
+// });
 
-module.exports = route;
+// module.exports = route;
